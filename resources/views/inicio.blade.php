a069d8b571e4dbe58fdc230d102b40cfe31dbe92
wesleynp1 <wesleynp987@gmail.com>
2025-02-22 21:41:34 -0300

    O Início de tudo

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>SisMarcacao</title>
+    </head>
+    <body>
+        <h1>Este é o Sistema de Marcação</h1>
+        <h2>O inicio de um trabalho</h2>
+        <p>A propósito, Hello World!</p>
+    </body>
+
+    <style>
+        body{
+            background-color: darkblue;
+            color: white;
+            text-align: center;
+        }
+    </style>
+</html>
